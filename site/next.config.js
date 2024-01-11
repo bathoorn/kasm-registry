cdddb4a7b9fb29e92a78a8dede552e0fc23d00a5
Ronnie Bathoorn <b@thoorn.com>
2024-01-11 16:44:08 +0100

    update config

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'bathoorn Kasm registry',
+    description: 'custom workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://bathoorn.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/bathoorn/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
